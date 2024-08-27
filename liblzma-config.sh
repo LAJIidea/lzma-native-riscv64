0aa6f2e4868b2ab459906b0210db31876502cd54
doboth <kingaobo@gmail.com>
2024-08-27 17:38:32 +0800

    fix riscv

--- liblzma-config.sh
+++ liblzma-config.sh
@@ -17,6 +17,6 @@ if [ $(uname) = "Darwin" -a $(uname -m) = "arm64" ]; then
     sed -i '' 's/\tnone)/\tarm64-*)\n\t\tbasic_machine=$(echo $basic_machine | sed "s\/arm64\/aarch64\/")\n\t\t;;\n\t\tnone)/g' $XZ_SRC_DIR/build-aux/config.sub
 fi
 
-sh xz-*/configure --enable-static --disable-shared --disable-scripts --disable-lzmainfo \
+sh xz-*/configure --build=riscv64-unknown-linux-gnu --enable-static --disable-shared --disable-scripts --disable-lzmainfo \
     --disable-lzma-links --disable-lzmadec --disable-xzdec --disable-xz --disable-rpath \
     --prefix="$TARGET_DIR/build" CFLAGS="$CFLAGS" >>node_liblzma_config.log 2>&1
